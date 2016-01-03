92b4ae1037f3b1dd53646f74194c5207108eb758
milindaperera <milindape@gmail.com>
2016-01-03 21:24:46 +1000

    add new class

---
+++ Samples/sample_01/loanService/facts/src/org/wso2/brs/sample/ResponseMessage.java
@@ -0,0 +1,17 @@
+package org.wso2.brs.sample;
+
+public class ResponseMessage {
+	
+	String message; //Response message to the applicant
+	
+	//Getters
+	public String getMessage() {
+		return message;
+	}
+	
+	//Setters
+	public void setMessage(String msg) {
+		this.message = msg;
+	}
+
+}
